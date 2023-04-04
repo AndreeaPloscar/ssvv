84648acc34438f2c85f5ca600676c39236e5c827
Ploscar Andreea <aploscar00@gmail.com>
2023-04-04 22:03:04 +0300

    add student test cases, fix validation

--- src/test/java/AddStudentUnitTests.java
+++ src/test/java/AddStudentUnitTests.java
@@ -1,7 +1,7 @@
 import domain.Nota;
 import domain.Student;
 import domain.Tema;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import repository.NotaXMLRepository;
@@ -13,8 +13,7 @@ import validation.StudentValidator;
 import validation.TemaValidator;
 import validation.Validator;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class AddStudentUnitTests {
@@ -28,24 +27,97 @@ public class AddStudentUnitTests {
         Validator<Tema> temaValidator = new TemaValidator();
         Validator<Nota> notaValidator = new NotaValidator();
 
-        fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
-        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
-        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
+        fileRepository1 = new StudentXMLRepository(studentValidator, "studenti_test.xml");
+        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme_test.xml");
+        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note_test.xml");
 
         service = new Service(fileRepository1, fileRepository2, fileRepository3);
     }
 
+    @AfterEach
+    void cleanUp(){
+        service.deleteStudent("1");
+    }
+
     @Test
-    void testAddStudentValid(){
-        var result = service.saveStudent("123", "name", 1234);
+    void testCase1(){
+        assertThrows(Exception.class, () -> {
+            var res = service.saveStudent("1", "nume", -1);
+            assertEquals(0, res);
+        });
+    }
+
+    @Test
+    void testCase2(){
+        assertThrows(Exception.class, () -> {
+            var res = service.saveStudent("1", "nume", 938);
+            assertEquals(0, res);
+        });
+    }
+
+    @Test
+    void testCase3(){
+        var result = service.saveStudent("1", "nume", 111);
         assertEquals(result, 1);
     }
 
     @Test
-    void testAddStudentInvalidGroup(){
-        var result = service.saveStudent("123", "name", -1234);
+    void testCase4(){
+        assertThrows(Exception.class, () -> {
+            var res = service.saveStudent("1", "nume", 0);
+            assertEquals(0, res);
+        });
+    }
+
+    @Test
+    void testCase6(){
+        assertThrows(Exception.class, () -> {
+            var res = service.saveStudent("", "nume", 111);
+            assertEquals(0, res);
+        });
+    }
+
+    @Test
+    void testCase7(){
+        assertThrows(Exception.class, () -> {
+            var res = service.saveStudent(null, "nume", 111);
+            assertEquals(0, res);
+        });
+    }
+
+    @Test
+    void testCase8(){
+        assertThrows(Exception.class, () -> {
+            var res = service.saveStudent("1", "", 111);
+            assertEquals(0, res);
+        });
+    }
+
+    @Test
+    void testCase9(){
+        assertThrows(Exception.class, () -> {
+            var res = service.saveStudent("1", null, 111);
+            assertEquals(0, res);
+        });
+    }
+
+    @Test
+    void testCase10(){
+        var result = service.saveStudent("1", "nume", 936);
+        assertEquals(result, 1);
+    }
+
+    @Test
+    void testCase11(){
+        var result = service.saveStudent("1", "nume", 937);
+        assertEquals(result, 1);
+    }
+
+    @Test
+    void testCase12(){
+        service.saveStudent("1", "nume", 937);
+        var result = service.saveStudent("1", "nume", 937);
         assertEquals(result, 0);
-        assertNull(fileRepository1.findOne("123"));
     }
 
 
